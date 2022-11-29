5341b770fb0fe65efce8ff120c56ab86e81962ae
johanjacobjo92 <johan.jacob@usi.ch>
2022-11-29 15:54:08 +0100

    switzerlandtour and json added
    Cant extract price out as its dependent on location need to ask Ivan

---
+++ test1/test1/spiders/tour_2.py
@@ -0,0 +1,30 @@
+import scrapy
+
+
+class EdxSpider(scrapy.Spider):
+    name = 'switzerlandtours'
+
+    def start_requests(self):
+        yield scrapy.Request(url="https://www.switzerland-tours.ch/tours/all-tours/",
+                             callback=self.parse)
+
+    def parse(self, response):
+        for tour_href in response.css('.title a::attr(href)').getall():
+            yield response.follow(tour_href, callback=self.parse_one_tour)
+
+    def parse_one_tour(self, tour_page_response):
+        title = tour_page_response.css('h1::text').get()
+        description = tour_page_response.css('p::text').get()
+        inclusions = tour_page_response.xpath('//*[@class="inclusions"]/ul/li//text()').getall()
+        exclusions = tour_page_response.xpath('//*[@class="exclusions"]/ul/li//text()').getall()
+        know_before_you_go = tour_page_response.xpath('//*[@class="knowbeforeyougo"]/ul/li//text()').getall()
+      
+
+        yield {
+            "title": title,
+            "Description ": description,
+            "inclusions": inclusions,
+            "exclusions": exclusions,
+            "know_before_you_go": know_before_you_go
+        }
+
