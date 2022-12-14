a4fd23ccc393555bf186a5a898618d624651ec82
Dylan Ramelli <rameld@usi.ch>
2022-12-14 10:58:37 +0100

    ...

--- UI/app.py
+++ UI/app.py
@@ -4,14 +4,20 @@ import query as q
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
         search_content = request.form['content']
+        website = request.form['swisstour'], request.form['getyourguide'], request.form['viator']
+        attribute = request.form['title'], request.form['description'], request.form[
+            'price'], request.form['rating'], request.form['included'], request.form['excluded']
         print(search_content)
+        print(website)
+        print(attribute)
 
         # Need to get the choice of website and attribute to filter for.
-        q.query(search_content)
+        # q.query(search_content)
         return redirect('/')
     else:
         return render_template('index.html')
