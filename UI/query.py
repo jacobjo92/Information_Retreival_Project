c205c84c1afec41f30c0959b4ff0ef995945d992
Dylan Ramelli <rameld@usi.ch>
2022-12-16 13:07:54 +0100

    ...

--- UI/query.py
+++ UI/query.py
@@ -1,8 +1,28 @@
 import requests
 def cleanup_string(input):
     input = input.replace("\n", "")
-    return input
-
+    return input                    
 # UI for querying the localhost.
+def query(choice, user_query, attribute):
+    
+    title = []
+    description = []
+    price = []
+    rating = []
+    
+    
+    URL = "http://localhost:8983/solr/" + choice + "/select?df="+attribute+"&indent=true&q.op=OR&q="+user_query
+    res = requests.get(URL)
+    if res.json()['response']['numFound'] > 0:
+        print("Number of results: " + str(res.json()['response']['numFound']))
+        for a in res.json()['response']['docs']:
+            if 'description' in a and 'title' in a and 'rating' in a and 'price' in a:
+                title.append(cleanup_string(str(a['title'][0])))
+                description.append(cleanup_string(str(a['description'][0])))
+                price.append(cleanup_string(str(a['price'][0])))
+                rating.append(cleanup_string(str(a['rating'][0])))
+        return title,description,price,rating
                     
                     
+    else:
+        print("No documents were found for your query.")
